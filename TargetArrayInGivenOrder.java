ccc30a252e93c6b46a3a78cbf06488ab61a27175
HammadHafeez186 <126067598+HammadHafeez186@users.noreply.github.com>
2023-09-07 12:30:46 +0500

    LeftAndRightSum.java file has been added

---
+++ TargetArrayInGivenOrder.java
@@ -0,0 +1,10 @@
+public class TargetArrayInGivenOrder {
+    public static void main(String[] args) {
+
+        int[] nums = {0,1,2,3,4};
+        int[] index = {}
+    }
+    public static int[] createTargetArray(int[] nums, int[] index) {
+
+    }
+}
