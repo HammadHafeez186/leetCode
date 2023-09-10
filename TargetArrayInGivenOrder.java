0c912769daf0076b62db1c470bd1b80830a49401
HammadHafeez186 <126067598+HammadHafeez186@users.noreply.github.com>
2023-09-10 20:30:57 +0500

    TargetArrayInGivenOrder.java added on 10/09/2023

--- TargetArrayInGivenOrder.java
+++ TargetArrayInGivenOrder.java
@@ -1,10 +1,32 @@
+import javax.swing.*;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.WeakHashMap;
+
 public class TargetArrayInGivenOrder {
     public static void main(String[] args) {
 
         int[] nums = {0,1,2,3,4};
-        int[] index = {}
+        int[] index = {0,1,2,2,1};
+
+        int[] arr = createTargetArray(nums,index);
+        for (int i: arr){
+            System.out.println(i);
+        }
     }
     public static int[] createTargetArray(int[] nums, int[] index) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        int[] target = new int[nums.length];
+        for(int i = 0; i < nums.length; i++){
 
+            arr.add(index[i],nums[i]);
+        }
+
+        for(int i = 0; i < arr.size(); i++){
+            target[i] = arr.get(i);
+        }
+
+        return target;
     }
 }
